286d8da06fd1a9621bbb0794970c29b3df5e2a25
TejasSai07 <147263041+TejasSai07@users.noreply.github.com>
2024-06-04 23:54:07 +0530

    Create L4_HeapSort.c

---
+++ LAB_6_Transform_and_Conquer/L4_HeapSort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+int opcount = 0;
+
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        heapify(arr, n, largest);
+    }
+}
+
+void buildHeap(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
+
+void heapSort(int arr[], int n) {
+    buildHeap(arr, n);
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    
+    buildHeap(arr, n);
+    
+
+    heapSort(arr, n);
+
+    printf("Opcount is %d",opcount);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
